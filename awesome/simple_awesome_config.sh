39333c8580c48dff28df21f67b9d08d564d5203e
Mehdi YAHIA CHERIF <44881216+Mehdi-YC@users.noreply.github.com>
2022-10-26 10:32:50 +0100

    Update simple_awesome_config.sh

--- awesome/simple_awesome_config.sh
+++ awesome/simple_awesome_config.sh
@@ -1,1 +1,6 @@
-
+ackagesNeeded='curl jq'
+if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
+elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
+elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
+elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
+else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
