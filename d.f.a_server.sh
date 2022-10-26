b40fe473c558d76dc8aeb281a780e30ce4e44112
Mehdi YAHIA CHERIF <44881216+Mehdi-YC@users.noreply.github.com>
2022-10-26 11:23:52 +0100

    Update d.f.a_server.sh

--- d.f.a_server.sh
+++ d.f.a_server.sh
@@ -40,3 +40,13 @@ sudo cp lazydocker /usr/bin/lazydocker
 ./docker.sh
 
 
+# portainer = all the rest (almost) add confirmation
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce
+# some docker-composes that iv tweaked
+cd 
+git clone https://github.com/Mehdi-YC/mydockerconfigs ./dockers 
