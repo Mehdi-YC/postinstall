a6a2070abeccfd2044b5553f7729db2cebdd113f
Mehdi YAHIA CHERIF <44881216+Mehdi-YC@users.noreply.github.com>
2022-10-26 10:16:30 +0100

    Update and rename simple_i3_gaps_arch.sh to simple_i3_gaps.sh

---
+++ i3/simple_i3_gaps.sh
@@ -0,0 +1,11 @@
+# search of apt , dnf or pacman
+
+
+# if pacman then install yay
+
+
+
+# clone the simple config of i3 and rofi and install lightdm witha good greeter
+
+
+# maybe adding nerdfont os good too
