a4f5076c4d5ffff1765fe1a9ef5bb529bff33678
Mehdi YAHIA CHERIF <44881216+Mehdi-YC@users.noreply.github.com>
2022-10-26 11:40:09 +0100

    Update and rename awesome/simple_awesome_config.sh to i3_awesome.sh

---
+++ i3_awesome.sh
@@ -0,0 +1,27 @@
+./d.f.a_server.sh
+
+awp='awesome lightdm xfce4-terminal thunar lxappearence firefox'
+i3p='i3 xfce4-terminal thunar lxappearence firefox'
+
+#desktop= with if else
+echo "select your window manager : \n \t 1 - awesomewm \n\t 2 -i3wm "
+read num
+
+if [[ $num -eq 1 ]]; then
+desktop=$awp
+else 
+desktop=$i3p
+fi
+
+if [ -x "$(command -v pacman)" ];       then 
+sudo pacman -Suy  xfce4-terminal thunar lxappearence firefox
+sudo yay -Suy i3-gaps lightdm awesome
+
+elif [ -x "$(command -v apt-get)" ]; then 
+sudo apt-get install $desktop
+
+elif [ -x "$(command -v dnf)" ];     then 
+sudo dnf install $desktop
+
+#elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
+else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
