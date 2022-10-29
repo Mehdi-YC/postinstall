d84d75957b7cd604aca9347192f7b703b11da3ef
Mehdi YAHIA CHERIF <44881216+Mehdi-YC@users.noreply.github.com>
2022-10-29 15:27:11 +0100

    Update d.f.a_server.sh

--- d.f.a_server.sh
+++ d.f.a_server.sh
@@ -1,14 +1,16 @@
 
 #maybe i can add taskwarrior and rustc
-packagesNeeded='tmux htop docker-compose python3-pip wget curl neovim git nodejs npm ansibel neofetch'
+packagesNeeded='tmux htop docker-compose python3-pip wget curl neovim git nodejs npm ansibel neofetch gcc'
 if [ -x "$(command -v pacman)" ];       then 
-sudo pacman -Suy $packagesNeeded go  docker
+sudo pacman -Suy $packagesNeeded
+sudo pacman -Suy go  docker
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
 makepkg -si
 
 elif [ -x "$(command -v apt)" ]; then 
-sudo apt-get install $packagesNeeded golang  docker.io
+sudo apt-get install $packagesNeeded
+sudo apt install golang  docker.io
 sudo apt remove neovim
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/stable
@@ -16,10 +18,11 @@ sudo apt-get update
 sudo apt install neovim
 
 elif [ -x "$(command -v dnf)" ];     then 
-sudo dnf install $packagesNeeded go  docker
+sudo dnf install $packagesNeeded 
+sudo dnf install go  docker
 
 elif [ -x "$(command -v zypper)" ];  then 
-sudo zypper install $packagesNeeded  docker
+sudo zypper install $packagesNeeded
 
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
 
@@ -46,12 +49,12 @@ sudo cp lazydocker /usr/bin/lazydocker
 
 
 # portainer = all the rest (almost) add confirmation
-sudo docker volume create portainer_data
-sudo docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer \
-    --restart=always \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer_data:/data \
-    portainer/portainer-ce
+#sudo docker volume create portainer_data
+#sudo docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer \
+#    --restart=always \
+#    -v /var/run/docker.sock:/var/run/docker.sock \
+#    -v portainer_data:/data \
+#    portainer/portainer-ce
 # some docker-composes that iv tweaked
 cd 
 git clone https://github.com/Mehdi-YC/mydockerconfigs ./dockers 
