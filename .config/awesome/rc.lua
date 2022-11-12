57aef08b2f5c41b34dcf08603f5cd961b4787efb
Mehdi YAHIA CHERIF <44881216+Mehdi-YC@users.noreply.github.com>
2022-11-12 16:55:33 +0100

    Update rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -571,3 +571,5 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+awful.util.spawn("compton")
