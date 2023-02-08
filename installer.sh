b978b5eda7ad3f90dac3d0476febe13c6ed2b13e
Mehdi YAHIA CHERIF <44881216+Mehdi-YC@users.noreply.github.com>
2023-02-08 10:37:25 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -59,7 +59,8 @@ curl -sS https://starship.rs/install.sh | sh #eval "$(starship init bash)"
 #lazydocker
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 sudo cp lazydocker /usr/bin/lazydocker
-
+#reload bash after installing cargo
+bash
 cargo install tokei
 cargo install --locked hyperfine
 cargo install bottom
