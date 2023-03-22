345bacaa28cb1a9955186ff92ccd652c6f82ff0e
Mehdi YAHIA CHERIF <44881216+Mehdi-YC@users.noreply.github.com>
2023-03-22 10:25:03 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -20,9 +20,18 @@ elif [ -x "$(command -v apt)" ]; then
 
 elif [ -x "$(command -v dnf)" ];     then 
   sudo dnf install $packagesNeeded 
-  sudo dnf install go  docker openssl-devel
+  sudo dnf install go  openssl-devel
+  #sudo dnf install docker
+  sudo dnf install podman
+  
   sudo dnf copr enable varlad/helix
   sudo dnf install helix
+  
+  sudo dnf copr enable alciregi/distrobox
+  sudo dnf install distrobox
+  
+  sudo dnf copr enable zeno/scrcpy
+  sudo dnf install scrcpy
 
 elif [ -x "$(command -v zypper)" ];  then 
   sudo zypper install $packagesNeeded
@@ -60,22 +69,25 @@ source ~/.bashrc
 #lazydocker
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 sudo cp lazydocker /usr/bin/lazydocker
+
 #reload bash after installing cargo
+source ~/.bashrc
 bash
+
 pip install howdoi
 cargo install tokei
 cargo install --locked hyperfine
-cargo install bottom
+#cargo install bottom
 cargo install mdcat
 cargo install --locked zellij
 cargo install --locked miniserve
 cargo install just
 cargo install --locked bat
-cargo install --locked broot
-cargo install htmlq
-cargo install skim # grep & fzf
+#cargo install --locked broot
+#cargo install htmlq
+#cargo install skim # grep & fzf
 cargo install hoard-rs
-pip install visidata
+#pip install visidata
 #python3 -m http.server
 
 # starship for bash
@@ -86,11 +98,11 @@ mkdir ~/.cache/starship
 starship init nu | save ~/.cache/starship/init.nu
 echo "source ~/.cache/starship/init.nu" | save --raw --append $nu.config-path
 
-echo -e "installing nerd fonts \n"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+#echo -e "installing nerd fonts \n"
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
 
 # Wallpapers :
-git clone https://github.com/linuxdotexe/nordic-wallpapers ~/.config/awesome/nordic-wallpapers 
+#git clone https://github.com/linuxdotexe/nordic-wallpapers ~/.config/awesome/nordic-wallpapers 
 
 
 
