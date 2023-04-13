9e40005316632b8ced4bc41a66c319c521402672
Mehdi YAHIA CHERIF <44881216+Mehdi-YC@users.noreply.github.com>
2023-04-13 17:14:42 +0100

    Update init.lua

--- distrobox_builder/.config/nvim/init.lua
+++ distrobox_builder/.config/nvim/init.lua
@@ -297,7 +297,8 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  --ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = {'lua', 'python', 'rust', 'bash' ,'javascript'},
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
