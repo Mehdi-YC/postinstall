3290812cc3614fe1f11d54cbb5e49e8a810d31d4
Mehdi YAHIA CHERIF <44881216+Mehdi-YC@users.noreply.github.com>
2023-04-16 11:17:50 +0100

    cleaning containerfile code

--- distrobox_builder/set_config.sh
+++ distrobox_builder/set_config.sh
@@ -1,25 +1,11 @@
 mkdir $HOME/.config/
 cp -r /usr/local/config/* $HOME/.config/
-
-
-cd # my omt mod
-git clone https://github.com/mehdi-yc/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
-
 mv ~/.config/.bashrc ~/.bashrc
-
-# GOOD CLI TOOLS : 
 cd && source ~/.bashrc
 
 #pip install howdoi
-#cargo install tokei zellij
-cargo install --locked hoard-rs nu
-# curl -sS https://starship.rs/install.sh | sh #eval "$(starship init bash)" currently installed with dnf
+
 sudo dnf copr enable atim/starship -y
-sudo dnf install starship -y
-# starship for nushell
-# mkdir ~/.cache/starship
-# nu
-# starship init nu | save ~/.cache/starship/init.nu
-# echo "source ~/.cache/starship/init.nu" | save --raw --append $nu.config-path
+sudo dnf copr enable atim/nushell -y
+sudo dnf install starship nushell -y
+
