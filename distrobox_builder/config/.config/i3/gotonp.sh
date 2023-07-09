65750f6b9b0f3e5ace483b4352e8ea1472cf87a9
Mehdi YAHIA CHERIF <44881216+Mehdi-YC@users.noreply.github.com>
2023-07-09 13:06:42 +0100

    sime i3 refactoring

--- distrobox_builder/config/.config/i3/gotonp.sh
+++ distrobox_builder/config/.config/i3/gotonp.sh
@@ -1,4 +1,13 @@
-wsNext=$(( $( i3-msg -t get_workspaces | jq '.[] | select(.focused).num' ) + $1))
-i3-msg move container to workspace $wsNext
-i3-msg workspace $wsNext
-echo "haha"
+#!/bin/bash
+
+# Get current workspace number
+current_ws=$(i3-msg -t get_workspaces | jq '.[] | select(.focused).num')
+
+# Calculate the next workspace number
+next_ws=$((current_ws + $1))
+
+# Move the container to the next workspace
+i3-msg move container to workspace "$next_ws"
+
+# Switch to the next workspace
+i3-msg workspace "$next_ws"
